e58fd9322dcccd865d2f6bedf915b0c79cd02fec
NazarChykur <nazar.chykur@emburse.com>
2022-09-04 10:50:57 +0300

    lesson_7 added xml based -> factory-method-injection

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -12,59 +12,20 @@ public class AppRunner {
         ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);  
         
-        /*
-               у дебаг режимі перевіряємо  ConnectionPool
-               
-                        IMEGES
-                            resources/images/lesson_6_xml-based_constructor-injection/constructor-injection.png
-                            
-                            
-                       connectionPool
-                            username = postgress
-                            poolSize = 10
-                            args    
-                                0 = --arg1=value1
-                                1 = --arg2=value2
-                            properties
-                                url -> postgresurl
-                                password -> 123
-         */
-
+//        з використанням фабричного методу
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
         
         /*
-        IMEGES
-            resources/images/lesson_6_xml-based_constructor-injection/construction-injection_indexedArgumentValues.png
-
-        context -> 
-                beanFactory -> 
-                        beanDefinitionMap ->
-                            pool1 -> {GenericBeanDefinition@1796} "Generic bean: class [com.study.spring.database.pool.ConnectionPool]; scope=; abstract=false; lazyInit=false; autowireMode=0; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=null; factoryMethodName=null; initMethodName=null; destroyMethodName=null; defined in class path resource [application.xml]"
-                            ->
-                                constructorArgumentValues -> 
-                                    indexedArgumentValues ->
-                                        {Integer@1799} 0 -> {ConstructorArgumentValues$ValueHolder@1800}
-                                        {Integer@1801} 3 -> {ConstructorArgumentValues$ValueHolder@1802}
-                                        {Integer@1803} 2 -> {ConstructorArgumentValues$ValueHolder@1804}
-                                        {Integer@1805} 1 -> {ConstructorArgumentValues$ValueHolder@1806}
+            IMEGES
+                resources/images/lesson_7_xml-based_factory-method-injection/factory-method-injection.png
+                
+                
+             у дебаг режимі перевіряємо  companyRepository  (так як всі біни по замовчуванню  SINGLETON , 
+                    то можемо бачити одинакоий код -> наприклад ConnectionPool@1676)
+                            
          */
         
-         /*
-        IMEGES
-            resources/images/lesson_6_xml-based_constructor-injection/construction-injection_genericArgumentValues.png
-
-        context -> 
-                beanFactory -> 
-                        beanDefinitionMap ->
-                            pool1 -> {GenericBeanDefinition@1796} "Generic bean: class [com.study.spring.database.pool.ConnectionPool]; scope=; abstract=false; lazyInit=false; autowireMode=0; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=null; factoryMethodName=null; initMethodName=null; destroyMethodName=null; defined in class path resource [application.xml]"
-                            ->
-                                constructorArgumentValues -> 
-                                    indexedArgumentValues -> 0
-                                    genericArgumentValues ->
-                                        0 - username
-                                        1 - properties
-                                        2 - args
-                                        3 - poolSize
-         */
 
     }
 }
--- src/main/java/com/study/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/study/spring/database/repository/CompanyRepository.java
@@ -6,7 +6,26 @@ public class CompanyRepository {
 
     private final ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
+//    public CompanyRepository(ConnectionPool connectionPool) {
+//        this.connectionPool = connectionPool;
+//    }
+    
+    /*
+        фабричний метод
+    effective java
+        про фабричний метод  описано ефективність його використання у книзі  effective java , але 
+        на практиці частіше використовується всерівно конструктор
+        
+                для використання фабричного методу, конструктор у класі зазвичай роблять приватним
+                і створюють статичний метод  of
+     */
+
+    private CompanyRepository(ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
+
+    public static CompanyRepository of(ConnectionPool connectionPool) {
+        return new CompanyRepository(connectionPool);
+    }
+    
 }
