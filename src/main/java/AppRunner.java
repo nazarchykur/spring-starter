c3e6efbaced2a19dd1ac819cc305f856915b81d0
NazarChykur <nazar.chykur@emburse.com>
2022-09-04 16:09:44 +0300

    lesson_2.8 added explanation of BeanFactoryPostProcessor in beans lifecycle

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -1,10 +1,32 @@
 import com.study.spring.database.pool.ConnectionPool;
 import com.study.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppRunner {
     public static void main(String[] args) {
+/*
+        оскільки він має бути першим у ції цепочці, то СПРІНГ його має перше найти і поставити у ції цепочці першим
+        для цього використовується  проста перевірка спеціального методу
+                isAssignableFrom
+                
+         тобто береться цей клас і перевіряється чи він звязаний з цим класом чи ні 
+         
+         якщо дуже покопатися у методі getBean(Class clazz)
+            то в дефолтній реалізації можна побачити Resolver   forRawClass()
+                де всередині через ClassUtils.isAssignable()  йде далі перевірка до методу    isAssignableFrom()
+                
+         це все для того аби ми дістали потрібні біти конкретного типу, тому такі біни потрібні для ініціалізації інших бінів
+         тому то така цепочка є у цьому IoC container     
+ */
+
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass())); // true
+
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass())); // false
         
+        
+
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
             ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
@@ -16,29 +38,40 @@ public class AppRunner {
         /*
         
         IMAGES
-                resources/images/lesson_2.7_injection_from_properties_files/properties-yaml files.png
-                resources/images/lesson_2.7_injection_from_properties_files/read from props file by key.png
-                resources/images/lesson_2.7_injection_from_properties_files/SPeL read beans and its fields.png
-                resources/images/lesson_2.7_injection_from_properties_files/read value form props using SPeL.png
+                resources/images/lesson_2.8_BeanFactoryPostProcessor/beans lifecycle + BeanFactoryPostProcessor.png
+
                 
          
          
-         Injection from properties files
+         BeanFactoryPostProcessor
          
-             Отже ще раз, у СПРІНГУ все крутиться довкола IoC container
-             
-             properties/yaml  file  це простий текстовий файл, який може мати свою структуру і мати код конвеншн 
-             
-             ці файли ми маємо надати для конфігурації (різні env, profiles, DB ... інші проперті)
-             так як це текстовий файл (не потрібно перекомпільовувати), то ми надаємо його для зчитування спрінгу
-             для цього у папці 
-                    resources/application.properties
+                public interface BeanFactoryPostProcessor {
+                        void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+                }
+                
+                
+         тепер зайдемо на його імплементацію, тобто клас 
+                        PropertySourcesPlaceholderConfigurer  postProcessBeanFactory() 
+               
+              якраз у цьому методі і відбувається ініціалізація проперті сорси і енвайромент 
+              де пізніше і підставляються значення з нашого файлу
+                        resources/application.properties
                     
                     
-              так як це приклад для xml, то додаємо ще один бін, де вказуємо де розташований наш файл або файли:
-                        <bean class="org.springframework.context.support.PropertySourcesPlaceholderConfigurer">
-                            <property name="location" value="application.properties"/>
-                        </bean>      
+                  
+
+         
+             Отже повертаємось до beans lifecycle, де до IoC container додаємо тепер BeanFactoryPostProcessor
+             
+                        images => resources/images/lesson_2.8_BeanFactoryPostProcessor/beans lifecycle + BeanFactoryPostProcessor.png
+                    
+             тобто тут СПРІНГ створює ці біни (їх може бути кілька), з допомогою якого ми читаємо всі потрібні значення 
+             з проперті файлів, а далі та сама цепочка, яку ми переглянули на попередніх уроках
+                    
+                    
+             оскільки  BeanFactoryPostProcessor  має бути першим у ції цепочці, то СПРІНГ його має перше найти і поставити у ції цепочці першим
+             для цього використовується  проста перевірка спеціального методу 
+                            isAssignableFrom     
                 
          */
 
