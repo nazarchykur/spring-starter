7771a01c9171830200ae6b45bf224f8c13744a9b
NazarChykur <nazar.chykur@emburse.com>
2022-09-04 11:47:03 +0300

    lesson_8 added xml based -> property injection

--- src/main/java/com/study/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/study/spring/database/pool/ConnectionPool.java
@@ -7,12 +7,23 @@ public class ConnectionPool {
     private final String username;
     private final Integer poolSize;
     private final List<Object> args;
-    private final Map<String, Object> properties;
+
+
+//    private final Map<String, Object> properties;
+    
+//    щоб використовувати ініціалізацію через  сетер  ми маємо забрати модифікатор  final
+//    в цьому є один із мінусів використання, тобто ми не можемо зробити наш об'єкт IMMUTABLE - НЕЗМІННИЙ
+//    другий мінус - можна зробити циклічність одного об'єкта на інший
+    private Map<String, Object> properties;
 
     public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
         this.username = username;
         this.poolSize = poolSize;
         this.args = args;
+        this.properties = properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
 }
