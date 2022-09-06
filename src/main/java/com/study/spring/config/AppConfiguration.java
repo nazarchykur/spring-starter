7b04fdbf867c38906c35b7b874e81215534ab913
NazarChykur <nazar.chykur@emburse.com>
2022-09-06 22:30:42 +0300

    lesson_4.2 added Java-based Configuration with @Import & @ImportResource

--- src/main/java/com/study/spring/config/AppConfiguration.java
+++ src/main/java/com/study/spring/config/AppConfiguration.java
@@ -1,7 +1,10 @@
 package com.study.spring.config;
 
+import com.study.web.config.WebConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 /*
@@ -21,6 +24,28 @@ import org.springframework.context.annotation.PropertySource;
                                         @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
                                 })
  */
+
+/*
+    @ImportResource
+        можна наприклад комбінувати наприклад + xml
+        
+        УВАГА: майже НЕ використовується, бо всі перейшли якраз на анотації
+        
+        @ImportResource("classpath:application.xml")
+ */
+
+/*
+   @Import()
+   
+       зазвичай тут вказуються ті конфіги які ми не скануємо тут , 
+       тобто це інші модулі, навіть у других джарках, інших залежностях які ми будемо підключати
+       
+       ось наприклад з іншого пакета, який ми не скануємо
+       
+            @Import(WebConfiguration.class)
+ */
+
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.study.spring")
---
+++ src/main/java/com/study/web/config/WebConfiguration.java
@@ -0,0 +1,7 @@
+package com.study.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+}
