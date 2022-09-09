12a556b5e4cb9f82c559a155714241de829c262e
NazarChykur <nazar.chykur@emburse.com>
2022-09-09 12:11:43 +0300

    lesson_6.6 added Spring-Boot Properties

--- src/main/java/com/study/AppRunner.java
+++ src/main/java/com/study/AppRunner.java
@@ -2,6 +2,7 @@ package com.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /*
     Одну інструкцію @SpringBootApplication можна використовувати для включення цих трьох функцій, а саме:
@@ -41,10 +42,132 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                              
         це для того щоб автосканувати вміст всіх папок для знаходження компонентів (всіх БІНІВ нашої програми)                      
  */
+
+/*
+     Properties
+        
+        https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
+        
+        application.properties використовуються для збереження «N» кількості властивостей в одному файлі для запуску програми. 
+        У Spring Boot властивості зберігаються у файлі application.properties у шляху до класу.   
+        
+            Файл application.properties знаходиться в каталозі src/main/resources . Зразок:
+                    server.port = 9090
+                    spring.application.name = demoservice
+                    
+        YAML File
+            Spring Boot підтримує налаштування властивостей на основі YAML для запуску програми. 
+            Замість application.properties ми можемо використовувати файл application.yml .
+                    spring:
+                       application:
+                          name: demoservice
+                    server:
+                       port: 8080                      
+       =================================================================================================================             
+           Використання анотації @Value
+                Анотація @Value використовується для читання значення властивості середовища або програми в коді Java. 
+                Синтаксис: 
+                        @Value("${property_key_name}")
+                приклад, у якому показано синтаксис читання значення властивості spring.application.name у змінній Java за допомогою анотації @Value.
+                        @Value("${spring.application.name}")     
+                        
+            
+       =================================================================================================================                 
+          Активний профіль Spring Boot
+          
+            Spring Boot підтримує різні властивості на основі активного профілю Spring. 
+            Наприклад, ми можемо зберегти два окремих файли для розробки та виробництва для запуску програми Spring Boot.
+            
+            Spring активний профіль у application.properties
+            Давайте розберемося, як мати активний профіль Spring у application.properties. 
+            За замовчуванням   application.properties   будуть використовуватися для запуску програми Spring Boot. 
+            Якщо ви хочете використовувати властивості на основі профілю, ми можемо зберегти окремий файл властивостей 
+            для кожного профілю, як показано нижче −    
+            
+            application.properties
+                    server.port = 8080
+                    spring.application.name = demoservice
+            -------------------------------------------------------------------------------        
+            application-dev.properties
+                    server.port = 9090
+                    spring.application.name = demoservice
+            ------------------------------------------------------------------------------- 
+            application-prod.properties
+                    server.port = 4431
+                    spring.application.name = demoservice              
+ */
+
+/*
+    https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.external-config
+            
+            Spring Boot uses a very particular PropertySource order that is designed to allow sensible overriding of values. 
+            Properties are considered in the following order (with values from lower items overriding earlier ones):
+
+        1. Default properties (specified by setting SpringApplication.setDefaultProperties).
+        
+        2. @PropertySource annotations on your @Configuration classes. Please note that such property sources are not added to the Environment until the application context is being refreshed. This is too late to configure certain properties such as logging.* and spring.main.* which are read before refresh begins.
+        
+        3. Config data (such as application.properties files).
+        
+        4. A RandomValuePropertySource that has properties only in random.*.
+        
+        5. OS environment variables.
+        
+        6. Java System properties (System.getProperties()).
+        
+        7. JNDI attributes from java:comp/env.
+        
+        8. ServletContext init parameters.
+        
+        9. ServletConfig init parameters.
+        
+        10. Properties from SPRING_APPLICATION_JSON (inline JSON embedded in an environment variable or system property).
+        
+        11. Command line arguments.
+        
+        12. properties attribute on your tests. Available on @SpringBootTest and the test annotations for testing a particular slice of your application.
+        
+        13. @TestPropertySource annotations on your tests.
+        
+        14. Devtools global settings properties in the $HOME/.config/spring-boot directory when devtools is active.
+        
+     !!!   
+     тобто визначивши ту саму проперті одним із способів (з цих 14), значення перезапишеться тим що йде нижче по списку  
+     
+     
+     
+     Config data files are considered in the following order:
+
+        1. Application properties packaged inside your jar (application.properties and YAML variants).
+        
+        2. Profile-specific application properties packaged inside your jar (application-{profile}.properties and YAML variants).
+        
+        3. Application properties outside of your packaged jar (application.properties and YAML variants).
+        
+        4. Profile-specific application properties outside of your packaged jar (application-{profile}.properties and YAML variants).
+        
+        
+        щоб подивитися що значення справді перезаписуються добавимо змінні через Intellij  -> Edit Configuration
+            
+      ==================================================================================================================
+      
+      Spring Boot - Using ${} placeholders in Property Files
+      
+        We just need to use ${someProp} in property file and start the application having 'someProp' in system 
+        properties or as main class (or jar) argument '--someProp=theValue'.
+
+        This feature allows us to use 'short' command line arguments.
+                app.title=Boot ${app}
+
+
+      
+ */
 @SpringBootApplication
 public class AppRunner {
     public static void main(String[] args) {
-        SpringApplication.run(AppRunner.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(AppRunner.class, args);
+        System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBean("pool1"));
     }
 
     /*
